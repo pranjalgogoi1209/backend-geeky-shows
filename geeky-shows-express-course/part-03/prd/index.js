9a8d78c8993f53d52ca850ecfb7d9b787bfca88c
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-07-20 21:03:51 +0530

    update

---
+++ geeky-shows-express-course/part-03/prd/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+// const express = require("express");
+
+var app = (0, _express["default"])();
+var port = 3000;
+app.get("/", function (req, res) {
+  res.send("Hello Pranjal Gogoi Test 12!");
+});
+app.listen(port, function () {
+  console.log("Example app listening on port ".concat(port));
+});
