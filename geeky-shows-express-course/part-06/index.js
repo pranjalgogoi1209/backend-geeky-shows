6c6c7346a02a6acd8c4c4c6258e212f9d3728db3
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-07-22 22:41:56 +0530

    update part-06

--- geeky-shows-express-course/part-06/index.js
+++ geeky-shows-express-course/part-06/index.js
@@ -1,27 +1,31 @@
-// Routing
-
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Routes
-
 app.get("/", (req, res) => {
   res.send("GET REQUEST");
 });
 
-/* app.all("/sabkuch", (req, res) => {
-  res.send("ALL REQUEST");
-}); */
+// String path
+app.get("/about", (req, res) => {
+  res.send("ABOUT PAGE");
+});
+
+// String pattern
+app.get("/ab?cd", (req, res) => {
+  res.send("This route path will match acd and abcd");
+});
 
 app.all("*", (req, res) => {
-  res.send("page not found !!!");
+  res.send("ALL REQUEST");
 });
 
-app.all("/api/*", (req, res) => {
-  res.send("page not found !!!");
-});
+// Catch-all route handler for any request that doesn't match the above routes
+/* app.use((req, res) => {
+  res.status(404).send("page not found !!!");
+}); */
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
