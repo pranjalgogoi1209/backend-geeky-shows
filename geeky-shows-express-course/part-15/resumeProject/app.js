d0a4d9e7bd4cd596c046f496d180a090ff6b3a89
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-07-30 10:45:16 +0530

    update

--- geeky-shows-express-course/part-15/resumeProject/app.js
+++ geeky-shows-express-course/part-15/resumeProject/app.js
@@ -1,17 +1,16 @@
 import express from "express";
+
 import homePage from "./routes/web.js";
-import aboutPage from "./routes/web.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // load routes
-app.use(homePage);
-app.use(aboutPage);
+app.get(homePage);
 
-// set template engine
+// set ejs middleware
 app.set("view engine", "ejs");
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`server is running at http://localhost:${port}`);
 });
