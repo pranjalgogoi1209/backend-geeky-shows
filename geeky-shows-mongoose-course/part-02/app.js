33b7db7cb8774d665890961f99f8ad425b623842
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-11-25 10:55:46 +0530

    completed mongoose part-02

--- geeky-shows-mongoose-course/part-02/app.js
+++ geeky-shows-mongoose-course/part-02/app.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 3000;
 }); */
 
 // database connection
+/* const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  "mongodb://pranjalgogoi:12345@localhost:27017/schooldb?authSource=schooldb";
+connectDB(DATABASE_URL); */
+
 const DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost:27017/schooldb";
 connectDB(DATABASE_URL);
--- geeky-shows-mongoose-course/part-02/db/connectdb.js
+++ geeky-shows-mongoose-course/part-02/db/connectdb.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async (DATABASE_URL) => {
   try {
-    await mongoose.connect(DATABASE_URL);
+    const DB_OPTIONS = {
+      user: "pranjalgogoi",
+      pass: "12345",
+      dbName: "schooldb",
+      authSource: "schooldb", // which db creates users
+    };
+    await mongoose.connect(DATABASE_URL, DB_OPTIONS);
     console.log("Connected to schooldb");
   } catch (error) {
     console.log(error);
